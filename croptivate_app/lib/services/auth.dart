259845302713e913122e10eecd089c6ad72e30f5
gailmaac <gailmac245@gmail.com>
2021-10-01 03:20:48 +0800

    Update auth.dart

--- croptivate_app/lib/services/auth.dart
+++ croptivate_app/lib/services/auth.dart
@@ -1,3 +1,4 @@
+import 'package:croptivate_app/services/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:croptivate_app/models/user_data.dart';
 
@@ -29,12 +30,54 @@ class AuthService {
   }
 
   //Sign In with Email and Password
-
+  Future signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebase(user!);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
   //Register with Email and Password - Seller
+  Future registerSeller(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+
+      //Create a new document for sellers with the uid
+      await DatabaseService(uid: user!.uid).updateUserData(" ", " ", " ", " ", " ", " ", 0);
+      return _userFromFirebase(user);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   //Register with Email and Password - Buyer
+  Future registerBuyer(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebase(user!);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   //Register with Email and Password - Transporter
+  Future registerTransporter(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebase(user!);
+    } catch(e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   //Sign out
   Future signOut() async {
