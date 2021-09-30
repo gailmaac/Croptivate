d6f6ba13af6ecc99d72e29000bac491896e523e2
gailmaac <gailmac245@gmail.com>
2021-09-30 13:03:23 +0800

    Create gesturedetector_cats.dart

---
+++ croptivate_app/lib/widgets/gesturedetector_cats.dart
@@ -0,0 +1,37 @@
+import 'package:croptivate_app/pallete.dart';
+import 'package:flutter/cupertino.dart';
+
+class GestureDetectorCats extends StatelessWidget {
+  const GestureDetectorCats({
+    Key? key,
+    required this.accounttype,
+  }) : super(key: key);
+
+  final String accounttype;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, 'RegisterSeller'),
+      child: Container(
+        height: size.height * 0.08,
+        width: size.width * 0.8,
+        child: Center(
+          child:Text(
+            accounttype,
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w900,
+              color: cWhite,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: cGreen),
+      ),
+    );
+  }
+}
