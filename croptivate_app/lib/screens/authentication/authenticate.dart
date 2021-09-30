a8afeef826df6a3e842630eb3f91ab07f4405dbe
gailmaac <gailmac245@gmail.com>
2021-10-01 03:20:29 +0800

    Update authenticate.dart

--- croptivate_app/lib/screens/authentication/authenticate.dart
+++ croptivate_app/lib/screens/authentication/authenticate.dart
@@ -1,3 +1,4 @@
+
 import 'package:croptivate_app/screens/authentication/sign_in.dart';
 import 'package:flutter/material.dart';
 
@@ -11,6 +12,7 @@ class Authenticate extends StatefulWidget {
 class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
+    
     return Container(
       child: SignIn(),
     );
