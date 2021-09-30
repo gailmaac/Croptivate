1e86283eeb776312122abe8969a5946aea0789eb
gailmaac <gailmac245@gmail.com>
2021-10-01 04:47:44 +0800

    Create user.dart

---
+++ croptivate_app/lib/models/user.dart
@@ -0,0 +1,22 @@
+class MyUser {
+
+  final String? uid;
+
+  MyUser({ this.uid });
+
+}
+
+class UserData {
+
+  final String uid;
+  final String sellerType;
+  final String fname;
+  final String lname;
+  final String loc;
+  final String shopname;
+  final String shopdesc;
+  final int cnum;
+
+  UserData({ required this.uid, required this.sellerType, required this.fname, required this.lname, required this.loc, required this.shopname, required this.shopdesc, required this.cnum});
+
+}
