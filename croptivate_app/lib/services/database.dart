f7c220331e099aca00d55aa5992bd1355aa0cd0c
gailmaac <gailmac245@gmail.com>
2021-10-01 22:22:07 +0800

    Update database.dart

--- croptivate_app/lib/services/database.dart
+++ croptivate_app/lib/services/database.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
 
 class DatabaseService {
 
@@ -6,18 +8,19 @@ class DatabaseService {
   DatabaseService({ required this.uid });
 
   //collection reference
-  final CollectionReference sellerCollection = FirebaseFirestore.instance.collection('userSeller');
-
-  Future updateUserData(String sellerType, String fname, String lname, String loc, String shopname, String shopdesc, int cnum) async {
-    return await sellerCollection.doc(uid).set({
-      'sellerType' : sellerType,
+  CollectionReference sellerCollection = FirebaseFirestore.instance.collection('userSeller');
+// String myInitialItem,
+  Future addUserSeller( String fname, String lname, String loc, String shopname, String shopdesc, int cnum) async {
+    return await sellerCollection
+    .add({//'myInitialItem' : myInitialItem,
       'fname' : fname,
       'lname' : lname,
       'loc' : loc,
       'shopname' : shopname,
       'shopdesc' : shopdesc,
-      'cnum' : cnum,
-    }); 
+      'cnum' : cnum,});
+      
+    
   }
 
   //Get Seller Stream
@@ -31,3 +34,4 @@ class DatabaseService {
     return sellerCollection.doc(uid).snapshots();
   }
 }
+
