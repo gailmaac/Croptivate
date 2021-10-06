96ff9af700c6cb817fd8a512cc94b755daf6a2fd
gailmaac <gailmac245@gmail.com>
2021-10-07 03:10:10 +0800

    Update wrapper.dart

--- croptivate_app/lib/screens/wrapper.dart
+++ croptivate_app/lib/screens/wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:croptivate_app/models/user.dart';
 import 'package:croptivate_app/screens/authentication/authenticate.dart';
 import 'package:croptivate_app/screens/home/home_buyer.dart';
+import 'package:croptivate_app/screens/sellers/home_seller.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -17,7 +18,7 @@ class Wrapper extends StatelessWidget {
     if (user == null) {
       return Authenticate();
     } else {
-      return Home();
+      return HomeSeller();
     }
     
 
