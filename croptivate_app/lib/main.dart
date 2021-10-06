ebd605c3bdef660f4006f1ebe5875c5adf4f214e
gailmaac <gailmac245@gmail.com>
2021-10-07 03:07:47 +0800

    Update main.dart

--- croptivate_app/lib/main.dart
+++ croptivate_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:croptivate_app/screens/authentication/account_categories.dart';
 import 'package:croptivate_app/screens/authentication/register_buyer.dart';
 import 'package:croptivate_app/screens/authentication/register_seller.dart';
 import 'package:croptivate_app/screens/authentication/register_transporter.dart';
+import 'package:croptivate_app/screens/buyers/basket.dart';
 import 'package:croptivate_app/screens/home/forgot_password.dart';
 import 'package:croptivate_app/screens/wrapper.dart';
 import 'package:croptivate_app/services/auth.dart';
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
           'RegisterSeller' : (context) => RegisterSeller(),
           'RegisterBuyer' : (context) => RegisterBuyer(),
           'RegisterTransporter' : (context) => RegisterTransporter(),
+          
         },
       ),
     );
