e5bcba192a0f4b159a82f594cdd5b03be04a7d1d
gailmaac <gailmac245@gmail.com>
2021-10-20 19:53:18 +0800

    Update product_listings.dart

--- croptivate_app/lib/screens/sellers/product_listings.dart
+++ croptivate_app/lib/screens/sellers/product_listings.dart
@@ -1,13 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:croptivate_app/pallete.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ProductPage extends StatelessWidget {
+class ProductPage extends StatefulWidget {
   const ProductPage({ Key? key }) : super(key: key);
 
   @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  late QuerySnapshot products;
+
+
+  
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+  return Scaffold(
       appBar: AppBar(
         title: Text("Product Listings"),
         backgroundColor: cGreen,
