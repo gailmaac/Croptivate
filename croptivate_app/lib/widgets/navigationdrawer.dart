f5507623a1b7f96c3355f12250951cf813fccc4e
gailmaac <gailmac245@gmail.com>
2021-10-22 22:54:04 +0800

    Update navigationdrawer.dart

--- croptivate_app/lib/widgets/navigationdrawer.dart
+++ croptivate_app/lib/widgets/navigationdrawer.dart
@@ -24,41 +24,27 @@ class NavigationDrawerWidget extends StatelessWidget {
         child: ListView(
           padding: padding,
           children: <Widget>[
-            const SizedBox(height: 48,),
-            buildMenuItem(
-              text: "Dashboard",
-              icon: Icons.space_dashboard_outlined,
-              onClicked: () => selectedItem(context, 0),
-            ),
-            const SizedBox(height: 16,),
+            const SizedBox(height: 260,),
             buildMenuItem(
               text: "Notifications",
               icon: Icons.notifications_none_rounded,
-              onClicked: () => selectedItem(context, 1),
+              onClicked: () => selectedItem(context, 0),
             ),
             const SizedBox(height: 16,),
             buildMenuItem(
               text: "Product Listings",
               icon: Icons.note_alt_outlined,
-              onClicked: () => selectedItem(context, 2),
+              onClicked: () => selectedItem(context, 1),
             ),
             const SizedBox(height: 16,),
             buildMenuItem(
               text: "Orders",
               icon: Icons.shopping_basket_outlined,
-              onClicked: () => selectedItem(context, 3),
+              onClicked: () => selectedItem(context, 2),
             ),
-            const SizedBox(height: 16,),
+            const SizedBox(height: 300,),
             Divider(color: Colors.black38,),
-
-            const SizedBox(height: 16,),
-            buildMenuItem(
-              text: "Settings",
-              icon: Icons.settings_outlined,
-              onClicked: () => selectedItem(context, 4),
-            ),
-
-            const SizedBox(height: 16,),
+            const SizedBox(height: 10,),
             buildMenuItem(
               text: "Sign Out",
               icon: Icons.logout_rounded,
@@ -103,18 +89,15 @@ Widget buildMenuItem({
 void selectedItem(BuildContext context, int index) {
   switch (index) {
     case 0:
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeSeller()));
-      break;
-    case 1:
       Navigator.of(context).push(MaterialPageRoute(builder: (context) => NotificationsPage()));
       break;
-    case 2:
+    case 1:
       Navigator.of(context).push(MaterialPageRoute(builder: (context) => ProductPage()));
       break;
-    case 3:
-      Navigator.of(context).push(MaterialPageRoute(builder: (context) => OrdersPage()));
+    case 2:
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomeSeller()));
       break;
-    case 4:
+    case 3:
       Navigator.of(context).push(MaterialPageRoute(builder: (context) => SettingsPage()));
       break;  
     default: break;
