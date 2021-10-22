6e62fb236cfb13fd6be8dce89da908228c2ac4bf
gailmaac <gailmac245@gmail.com>
2021-10-22 22:53:48 +0800

    Create edit_profile.dart

---
+++ croptivate_app/lib/screens/sellers/edit_profile.dart
@@ -0,0 +1,41 @@
+import 'package:croptivate_app/pallete.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EditProfile extends StatefulWidget {
+  const EditProfile({ Key? key }) : super(key: key);
+
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text("Edit my Profile", 
+        style: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 24,
+            color: cGreen,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+          Navigator.pop(context);
+          },
+            icon: Icon(
+            Icons.arrow_back_ios,
+            color: cBlack,
+            size: 15,
+          ),
+        ),
+      )
+    );
+  }
+}
