25a67034c1bc32971890149abc3e711168c080a6
gailmaac <gailmac245@gmail.com>
2021-10-26 03:13:52 +0800

    Create basket_model.dart

---
+++ croptivate_app/lib/models/basket_model.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:croptivate_app/models/product_model.dart';
+import 'package:equatable/equatable.dart';
+
+class Basket extends Equatable {
+  final List<Product> products;
+  
+  const Basket({this.products = const <Product>[]});
+
+
+  double get subtotal => 
+  products.fold(0, (total, current) => total + current.price);
+
+  double shippingFee(subtotal) {
+    if(subtotal >= 500.0) {
+      return 0.0;
+    } else {
+      return 100.0;
+    }
+  }
+
+  double total(subtotal, shippingFee) {
+    return subtotal + shippingFee(subtotal);
+  }
+
+  String get subtotalString => subtotal.toStringAsFixed(2);
+
+  String get shippingFeeString => shippingFee(subtotal).toStringAsFixed(2);
+
+  String get totalString => total(subtotal, shippingFee).toStringAsFixed(2);
+
+  
+
+  @override
+  List<Object?> get props => [products];
+}
