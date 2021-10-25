70d217db4072bf0bc789824fd900d032e1b2685f
gailmaac <gailmac245@gmail.com>
2021-10-26 03:13:19 +0800

    Create basket_event.dart

---
+++ croptivate_app/lib/blocs/basket/basket_event.dart
@@ -0,0 +1,31 @@
+part of 'basket_bloc.dart';
+
+abstract class BasketEvent extends Equatable{
+  const BasketEvent();
+
+  @override 
+  List<Object> get props => [];
+}
+
+class BasketStarted extends BasketEvent {
+  @override 
+  List<Object> get props => [];
+}
+
+class BasketProductAdded extends BasketEvent {
+  final Product product;
+
+  const BasketProductAdded(this.product);
+  @override 
+  List<Object> get props => [product];
+}
+
+class BasketProductRemoved extends BasketEvent {
+  final Product product;
+
+  const BasketProductRemoved(this.product);
+
+  @override 
+  List<Object> get props => [product];
+}
+
