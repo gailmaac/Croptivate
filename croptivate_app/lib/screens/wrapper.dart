9407daefe790000f7c865e3027fb7e9c1b9ce80f
gailmaac <gailmac245@gmail.com>
2021-10-26 03:16:23 +0800

    Update wrapper.dart

--- croptivate_app/lib/screens/wrapper.dart
+++ croptivate_app/lib/screens/wrapper.dart
@@ -1,6 +1,5 @@
 import 'package:croptivate_app/models/user.dart';
 import 'package:croptivate_app/screens/authentication/authenticate.dart';
-import 'package:croptivate_app/screens/home/home_buyer.dart';
 import 'package:croptivate_app/screens/sellers/home_seller.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
