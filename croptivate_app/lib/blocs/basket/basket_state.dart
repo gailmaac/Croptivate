ed300efb40b702a29d65ad9d6016f8174ab6dabf
gailmaac <gailmac245@gmail.com>
2021-10-26 03:13:26 +0800

    Create basket_state.dart

---
+++ croptivate_app/lib/blocs/basket/basket_state.dart
@@ -0,0 +1,28 @@
+part of 'basket_bloc.dart';
+
+abstract class BasketState extends Equatable{
+  const BasketState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class BasketLoading extends BasketState {
+  @override
+  List<Object?> get props => [];
+}
+
+class BasketLoaded extends BasketState {
+  final Basket basket;
+
+  const BasketLoaded({this.basket = const Basket()});
+
+  @override
+  List<Object?> get props => [basket];
+}
+
+class BasketError extends BasketState {
+
+  @override
+  List<Object?> get props => [];
+}
