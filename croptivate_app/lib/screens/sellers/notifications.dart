06c87f64255c1be4bd95042741969e6b8af3b711
gailmaac <gailmac245@gmail.com>
2021-10-26 03:16:16 +0800

    Update notifications.dart

--- croptivate_app/lib/screens/sellers/notifications.dart
+++ croptivate_app/lib/screens/sellers/notifications.dart
@@ -2,9 +2,20 @@ import 'package:croptivate_app/pallete.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
   const NotificationsPage({ Key? key }) : super(key: key);
 
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+  static const String routeName = '/notifications';
+  static Route route() {
+  return MaterialPageRoute(
+    settings: RouteSettings(name: routeName),
+    builder: (_) => NotificationsPage());
+  }
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
