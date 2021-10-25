a06659d10452c6a0062030bc0ec6de214fa24dca
gailmaac <gailmac245@gmail.com>
2021-10-26 03:13:47 +0800

    Update main.dart

--- croptivate_app/lib/main.dart
+++ croptivate_app/lib/main.dart
@@ -1,18 +1,13 @@
+import 'package:croptivate_app/blocs/basket/basket_bloc.dart';
+import 'package:croptivate_app/blocs/favorites/favorites_bloc.dart';
+import 'package:croptivate_app/config/app_router.dart';
 import 'package:croptivate_app/models/user.dart';
-import 'package:croptivate_app/screens/authentication/account_categories.dart';
-import 'package:croptivate_app/screens/authentication/register_buyer.dart';
-import 'package:croptivate_app/screens/authentication/register_seller.dart';
-import 'package:croptivate_app/screens/authentication/register_transporter.dart';
-import 'package:croptivate_app/screens/buyers/basket.dart';
-import 'package:croptivate_app/screens/buyers/user_profile.dart';
-import 'package:croptivate_app/screens/home/forgot_password.dart';
-import 'package:croptivate_app/screens/home/home_buyer.dart';
-import 'package:croptivate_app/screens/sellers/home_seller.dart';
-import 'package:croptivate_app/screens/splashscreen.dart';
+import 'package:croptivate_app/screens/authentication/sign_in.dart';
 import 'package:croptivate_app/screens/wrapper.dart';
 import 'package:croptivate_app/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
@@ -22,27 +17,35 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return StreamProvider<MyUser?>.value(
       initialData: null,
       value: AuthService().user,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Wrapper(),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => FavoritesBloc()..add(StartFavorites())),
+          BlocProvider(create: (_) => BasketBloc()..add(BasketStarted())),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          // home: Wrapper(),
 
-        routes: {
-          'ForgotPassword' : (context) => ForgotPassword(),
-          'AccountCategories' : (context) => AccountCategories(),
-          'RegisterSeller' : (context) => RegisterSeller(),
-          'RegisterBuyer' : (context) => RegisterBuyer(),
-          'RegisterTransporter' : (context) => RegisterTransporter(),
-          UserProfile.route: (context) => UserProfile(),
-          HomeSeller.route: (context) => HomeSeller(),
-          HomeBuyer.route: (context) => HomeBuyer(),
-        },
+          onGenerateRoute: AppRouter.onGenerateRoute,
+          initialRoute: SignIn.routeName,
+
+          // routes: {
+          //   'ForgotPassword' : (context) => ForgotPassword(),
+          //   'AccountCategories' : (context) => AccountCategories(),
+          //   'RegisterSeller' : (context) => RegisterSeller(),
+          //   'RegisterBuyer' : (context) => RegisterBuyer(),
+          //   'RegisterTransporter' : (context) => RegisterTransporter(),
+          //   UserProfile.route: (context) => UserProfile(),
+          //   HomeSeller.route: (context) => HomeSeller(),
+          //   // HomeBuyer.route: (context) => HomeBuyer(),
+          // },
+        ),
       ),
     );
   }
