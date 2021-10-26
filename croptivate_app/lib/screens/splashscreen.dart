15e714c438a30009b14065040a13ad7fd3ee8093
gailmaac <gailmac245@gmail.com>
2021-10-27 01:58:19 +0800

    Update splashscreen.dart

--- croptivate_app/lib/screens/splashscreen.dart
+++ croptivate_app/lib/screens/splashscreen.dart
@@ -1,37 +1,35 @@
-// import 'dart:async';
-// import 'package:croptivate_app/screens/wrapper.dart';
-// import 'package:croptivate_app/widgets/backgroundimage.dart';
-// import 'package:croptivate_app/screens/home/home_buyer.dart';
-// import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:croptivate_app/screens/wrapper.dart';
+import 'package:croptivate_app/widgets/backgroundimage.dart';
+import 'package:flutter/material.dart';
 
 
-// class SplashScreen extends StatefulWidget {
-//   const SplashScreen({ Key? key }) : super(key: key);
+class SplashScreen extends StatefulWidget {
 
-//   @override
-//   _SplashScreenState createState() => _SplashScreenState();
-// }
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
 
-// class _SplashScreenState extends State<SplashScreen> {
-//   startTimer() {
-//     Timer(Duration(seconds:4), () async {
-//       Route newRoute = MaterialPageRoute(builder: (context) => Wrapper());
-//       Navigator.pushReplacement(context, newRoute);
-//     });
-//   }
+  static const String routeName = '/splash';
+  
+  static Route route() {
+    return MaterialPageRoute(
+      settings: RouteSettings(name: routeName),
+      builder: (_) => SplashScreen());
+  }
+}
 
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     startTimer();
-//   }
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       children: [
-//         Image.asset("assets\splashscreen.png"),
-//       ]
-//     );
-//   }
-// }
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Timer(Duration(seconds: 4), () => Navigator.pushNamed(context, '/'));
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image.asset(
+        "assets/splashscreen.png",
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
 
