0c176131715990a6d00dec7a31e0e94f5e0bccd9
gailmaac <gailmac245@gmail.com>
2021-11-01 23:51:37 +0800

    Update product_model.dart

--- croptivate_app/lib/models/product_model.dart
+++ croptivate_app/lib/models/product_model.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 class Product extends Equatable {
   final String name;
   final String category;
-  final String imageUrl;
+  final String imageUrlOne;
+  final String imageUrlTwo;
+  final String imageUrlThree;
   final double price;
   final bool isDeals;
   final bool isRecommended;
@@ -13,7 +16,9 @@ class Product extends Equatable {
   const Product({
     required this.name,
     required this.category,
-    required this.imageUrl,
+    required this.imageUrlOne,
+    required this.imageUrlTwo,
+    required this.imageUrlThree,
     required this.price,
     required this.isDeals,
     required this.isRecommended,
@@ -21,11 +26,29 @@ class Product extends Equatable {
     required this.stockCount
   });
 
+  static Product fromSnapshot(DocumentSnapshot snap){
+    Product product = Product(
+      name: snap['prodname'],
+      category: snap['myInitialCat'],
+      imageUrlOne: snap['mediaUrlOne'],
+      imageUrlTwo: snap['mediaUrlTwo'],
+      imageUrlThree: snap['mediaUrlThree'],
+      price: snap['retail'],
+      isDeals: snap['isDeals'],
+      isRecommended: snap['isRecommended'],
+      location: snap['location'],
+      stockCount: snap['stock'],
+      );
+      return product;
+  }
+
   @override
   List<Object?> get props => [
     name,
     category,
-    imageUrl,
+    imageUrlOne,
+    imageUrlTwo,
+    imageUrlThree,
     price,
     isDeals,
     isRecommended,
@@ -38,7 +61,9 @@ class Product extends Equatable {
     Product(
       name: "Potato Marbles",
       category: "Root Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1508313880080-c4bef0730395?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1508313880080-c4bef0730395?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1508313880080-c4bef0730395?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1508313880080-c4bef0730395?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
       price: 90,
       isDeals: true,
       isRecommended: false,
@@ -48,7 +73,9 @@ class Product extends Equatable {
     Product(
       name: "Potato",
       category: "Root Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1518977676601-b53f82aba655?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
       price: 70,
       isDeals: true,
       isRecommended: false,
@@ -58,7 +85,9 @@ class Product extends Equatable {
     Product(
       name: "Chayote",
       category: "Climbers and Creepers",
-      imageUrl: "https://as1.ftcdn.net/v2/jpg/00/60/28/62/1000_F_60286292_5jwoJaa1TWanYFLJXf7eOeKlg4iL8ad3.jpg",
+      imageUrlOne: "https://as1.ftcdn.net/v2/jpg/00/60/28/62/1000_F_60286292_5jwoJaa1TWanYFLJXf7eOeKlg4iL8ad3.jpg",
+      imageUrlTwo: "https://as1.ftcdn.net/v2/jpg/00/60/28/62/1000_F_60286292_5jwoJaa1TWanYFLJXf7eOeKlg4iL8ad3.jpg",
+      imageUrlThree: "https://as1.ftcdn.net/v2/jpg/00/60/28/62/1000_F_60286292_5jwoJaa1TWanYFLJXf7eOeKlg4iL8ad3.jpg",
       price: 100,
       isDeals: true,
       isRecommended: false,
@@ -68,7 +97,9 @@ class Product extends Equatable {
     Product(
       name: "Pechay",
       category: "Leafy Vegetables",
-      imageUrl: "https://as2.ftcdn.net/v2/jpg/03/26/95/93/1000_F_326959353_wlJd8opIhFrL97RKVNuvaobFwHp6PaBB.jpg",
+      imageUrlOne: "https://as2.ftcdn.net/v2/jpg/03/26/95/93/1000_F_326959353_wlJd8opIhFrL97RKVNuvaobFwHp6PaBB.jpg",
+      imageUrlTwo: "https://as2.ftcdn.net/v2/jpg/03/26/95/93/1000_F_326959353_wlJd8opIhFrL97RKVNuvaobFwHp6PaBB.jpg",
+      imageUrlThree: "https://as2.ftcdn.net/v2/jpg/03/26/95/93/1000_F_326959353_wlJd8opIhFrL97RKVNuvaobFwHp6PaBB.jpg",
       price: 70,
       isDeals: true,
       isRecommended: false,
@@ -78,7 +109,9 @@ class Product extends Equatable {
     Product(
       name: "Bottle Gourd",
       category: "Climbers and Creepers",
-      imageUrl: "https://th.bing.com/th/id/R.f5277d0d60acf5a9952ff0b14b0f3a72?rik=pE6%2fm%2fW9b14XTw&riu=http%3a%2f%2fwww.diyhealthremedy.com%2fwp-content%2fuploads%2f2014%2f08%2fBottle-Gourd.jpg&ehk=htjp%2bEeXvGCUCjP8q%2bfp2WSCl%2ftPeI%2bhywzRMTt3cro%3d&risl=&pid=ImgRaw&r=0",
+      imageUrlOne: "https://th.bing.com/th/id/R.f5277d0d60acf5a9952ff0b14b0f3a72?rik=pE6%2fm%2fW9b14XTw&riu=http%3a%2f%2fwww.diyhealthremedy.com%2fwp-content%2fuploads%2f2014%2f08%2fBottle-Gourd.jpg&ehk=htjp%2bEeXvGCUCjP8q%2bfp2WSCl%2ftPeI%2bhywzRMTt3cro%3d&risl=&pid=ImgRaw&r=0",
+      imageUrlTwo: "https://th.bing.com/th/id/R.f5277d0d60acf5a9952ff0b14b0f3a72?rik=pE6%2fm%2fW9b14XTw&riu=http%3a%2f%2fwww.diyhealthremedy.com%2fwp-content%2fuploads%2f2014%2f08%2fBottle-Gourd.jpg&ehk=htjp%2bEeXvGCUCjP8q%2bfp2WSCl%2ftPeI%2bhywzRMTt3cro%3d&risl=&pid=ImgRaw&r=0",
+      imageUrlThree: "https://th.bing.com/th/id/R.f5277d0d60acf5a9952ff0b14b0f3a72?rik=pE6%2fm%2fW9b14XTw&riu=http%3a%2f%2fwww.diyhealthremedy.com%2fwp-content%2fuploads%2f2014%2f08%2fBottle-Gourd.jpg&ehk=htjp%2bEeXvGCUCjP8q%2bfp2WSCl%2ftPeI%2bhywzRMTt3cro%3d&risl=&pid=ImgRaw&r=0",
       price: 120,
       isDeals: true,
       isRecommended: false,
@@ -88,7 +121,9 @@ class Product extends Equatable {
     Product(
       name: "Bell Pepper",
       category: "Plant Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1601648764658-cf37e8c89b70?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80",
       price: 110,
       isDeals: true,
       isRecommended: false,
@@ -98,7 +133,9 @@ class Product extends Equatable {
     Product(
       name: "Red Leaf Lettuce",
       category: "Leafy Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1591981093714-034c8ed010f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1591981093714-034c8ed010f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1591981093714-034c8ed010f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1591981093714-034c8ed010f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
       price: 130,
       isDeals: true,
       isRecommended: false,
@@ -110,7 +147,9 @@ class Product extends Equatable {
       Product(
       name: "Broccoli",
       category: "Leafy Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1606585333304-a7fa1ca4376c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1606585333304-a7fa1ca4376c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1591981093714-034c8ed010f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1591981093714-034c8ed010f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
       price: 100,
       isDeals: false,
       isRecommended: true,
@@ -120,7 +159,9 @@ class Product extends Equatable {
     Product(
       name: "Cabbage",
       category: "Climbers and Creepers",
-      imageUrl: "https://images.unsplash.com/photo-1603049404411-13c2ca81a316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=882&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1603049404411-13c2ca81a316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=882&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1603049404411-13c2ca81a316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=882&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1603049404411-13c2ca81a316?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=882&q=80",
       price: 90,
       isDeals: false,
       isRecommended: true,
@@ -130,7 +171,9 @@ class Product extends Equatable {
     Product(
       name: "Cauliflower",
       category: "Climbers and Creepers",
-      imageUrl: "https://images.unsplash.com/photo-1601171908684-75604cf5da5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1601171908684-75604cf5da5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1601171908684-75604cf5da5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1601171908684-75604cf5da5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
       price: 100,
       isDeals: false,
       isRecommended: true,
@@ -140,7 +183,9 @@ class Product extends Equatable {
     Product(
       name: "Carrots",
       category: "Root Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1447175008436-054170c2e979?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1447175008436-054170c2e979?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1447175008436-054170c2e979?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1447175008436-054170c2e979?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80",
       price: 70,
       isDeals: false,
       isRecommended: true,
@@ -150,7 +195,9 @@ class Product extends Equatable {
     Product(
       name: "Bitter Gourd",
       category: "Climbers and Creepers",
-      imageUrl: "https://images.unsplash.com/photo-1588391453522-a8b470845269?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1526&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1588391453522-a8b470845269?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1526&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1588391453522-a8b470845269?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1526&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1588391453522-a8b470845269?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1526&q=80",
       price: 90,
       isDeals: false,
       isRecommended: true,
@@ -160,7 +207,9 @@ class Product extends Equatable {
     Product(
       name: "Tomato",
       category: "Plant Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1582284540020-8acbe03f4924?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1582284540020-8acbe03f4924?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1582284540020-8acbe03f4924?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1582284540020-8acbe03f4924?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80",
       price: 60,
       isDeals: false,
       isRecommended: true,
@@ -170,7 +219,9 @@ class Product extends Equatable {
     Product(
       name: "Lady Finger",
       category: "Plant Vegetables",
-      imageUrl: "https://images.unsplash.com/photo-1558408525-1092038389ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=580&q=80",
+      imageUrlOne: "https://images.unsplash.com/photo-1558408525-1092038389ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=580&q=80",
+      imageUrlTwo: "https://images.unsplash.com/photo-1558408525-1092038389ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=580&q=80",
+      imageUrlThree: "https://images.unsplash.com/photo-1558408525-1092038389ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=580&q=80",
       price: 50,
       isDeals: false,
       isRecommended: true,
