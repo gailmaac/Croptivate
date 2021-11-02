91ed3615ca34fbd3a60a39fe30b4f4dc9ebc2293
gailmaac <gailmac245@gmail.com>
2021-11-03 00:21:05 +0800

    Update home_seller.dart

--- croptivate_app/lib/screens/sellers/home_seller.dart
+++ croptivate_app/lib/screens/sellers/home_seller.dart
@@ -60,19 +60,19 @@ class _HomeSellerState extends State<HomeSeller> {
               children: [
                 Container(
                   height: double.infinity,
-                  color: Colors.brown[800],
+                  color: cWhite,
                 ),
                 Container(
                   height: double.infinity,
-                  color: cDGreen,
+                  color: cWhite,
                 ),
                 Container(
                   height: double.infinity,
-                  color: cGrey,
+                  color: cWhite,
                 ),
                 Container(
                   height: double.infinity,
-                  color: cBlack,
+                  color: cWhite,
                 ),
 
               ]
