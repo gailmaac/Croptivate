418774bd6b28ce1afe710d9f0e6c3c06ebbc451c
gailmaac <gailmac245@gmail.com>
2021-11-04 12:55:36 +0800

    Update user_profile.dart

--- croptivate_app/lib/screens/buyers/user_profile.dart
+++ croptivate_app/lib/screens/buyers/user_profile.dart
@@ -10,219 +10,24 @@ import 'package:croptivate_app/shared/loading.dart';
 import 'package:croptivate_app/services/database.dart';
 
 
-class UserProfile extends StatelessWidget {
+// class UserProfile extends StatelessWidget {
 
-  static const String routeName = '/userprofilebuyer';
+//   static const String routeName = '/userprofilebuyer';
   
-  static Route route({required User user}) {
-    return MaterialPageRoute(
-        settings: RouteSettings(name: routeName),
-        builder: (_) => UserProfile(user: user));
-  }
-
-  final User user;
-  const UserProfile({required this.user});
-
-  
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text("My Profile", 
-        style: TextStyle(
-            fontFamily: 'Poppins',
-            color: cGreen,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-                onPressed: () {
-                Navigator.pop(context);
-                },
-                  icon: Icon(
-                  Icons.arrow_back_ios,
-                  color: cBlack,
-                  size: 15,
-                  ),
-                ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfile()));
-            },
-            color: cBlack,
-            icon: Icon(Icons.settings_outlined),
-          )
-        ],
-      ),
-      body: ListView(
-        children: [
-          Column(
-            children: [
-              Text(
-                user.fname + user.lname,
-                style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Poppins',
-                color: cBlack,
-                fontSize: 24
-              ),
-              )
-            ]
-          ),
-          const SizedBox(height: 24),
-          trackOrderbutton(),
-          const SizedBox(height: 10),
-          myFavsbutton(),
-          const SizedBox(height: 10),
-          startSellbutton(),
-          const SizedBox(height: 170),
-          signOutButton()
-        ],
-      )
-    );
-  }
-
-  //Track Order Button
-  Widget trackOrderbutton() {
-    return TextButton(
-      onPressed: () {
-
-      }, 
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          children: [
-            Icon(Icons.location_on_outlined, color: cGreen),
-            SizedBox(width: 30),
-            Text(
-                "Track my Order",
-              style: TextStyle(fontSize: 20,
-              color: cBlack,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-              )
-              ),
-          ],
-        ),
-        color: Colors.transparent,
-        width: double.infinity,
-        height: 65,
-      )
-    );
-  }
-
-  //Favorites Button
-  Widget myFavsbutton() {
-    return TextButton(
-      onPressed: () {
-
-      }, 
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          children: [
-            Icon(Icons.favorite_border_rounded, color: cGreen),
-            SizedBox(width: 30),
-            Text(
-                "My Favorites",
-              style: TextStyle(fontSize: 20,
-              color: cBlack,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-              )
-              ),
-          ],
-        ),
-        color: Colors.transparent,
-        width: double.infinity,
-        height: 65,
-      )
-    );
-  }
-
-//Start Selling Button
-  Widget startSellbutton() {
-    return TextButton(
-      onPressed: () {
-
-      }, 
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Row(
-          children: [
-            Icon(Icons.storefront_outlined, color: cGreen),
-            SizedBox(width: 30),
-            Text(
-                "Start Selling",
-              style: TextStyle(fontSize: 20,
-              color: cBlack,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-              )
-              ),
-          ],
-        ),
-        color: Colors.transparent,
-        width: double.infinity,
-        height: 65,
-      )
-    );
-  }
-
-  //Sign Out Button
-  Widget signOutButton() {
-    return TextButton(
-      onPressed: () async {
-        // await _auth.signOut();
-      }, 
-      child: Container(
-        decoration: BoxDecoration(color: cGreen,
-        borderRadius: BorderRadius.circular(12)),
-        padding: EdgeInsets.all(20),
-        child: Row(
-          children: [
-            Icon(Icons.logout_rounded, color: cWhite),
-            SizedBox(width: 30),
-            Text(
-                "Sign Out",
-              style: TextStyle(fontSize: 20,
-              color: cWhite,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-              )
-              ),
-          ],
-        ),
-        width: 350,
-        height: 65,
-      )
-    );
-  }
-}
-
-// class UserProfile extends StatefulWidget {
-//   static String route = "userprofile";
-
-
-
-//   @override
-//   _UserProfileState createState() => _UserProfileState();
-// }
-
-//   bool loading = false;
+//   static Route route({required User user}) {
+//     return MaterialPageRoute(
+//         settings: RouteSettings(name: routeName),
+//         builder: (_) => UserProfile(user: user));
+//   }
 
 //   final User user;
 //   const UserProfile({required this.user});
+
   
-// class _UserProfileState extends State<UserProfile> {
+
 //   @override
 //   Widget build(BuildContext context) {
+    
 //     return Scaffold(
 //       appBar: AppBar(
 //         backgroundColor: Colors.transparent,
@@ -256,21 +61,19 @@ class UserProfile extends StatelessWidget {
 //         ],
 //       ),
 //       body: ListView(
-//         // physics: BouncingScrollPhysics(),
 //         children: [
-//           // ProfileWidget(
-//           //   imagePath: userOne.imagePath,
-//           //   onClicked: () async {
-              
-//           //   }
-//           // ),
-//           // const SizedBox(height: 24),
 //           Column(
 //             children: [
 //               Text(
-//                 user.fname +
-//               ))
-//             ],
+//                 user.fname + user.lname,
+//                 style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 fontFamily: 'Poppins',
+//                 color: cBlack,
+//                 fontSize: 24
+//               ),
+//               )
+//             ]
 //           ),
 //           const SizedBox(height: 24),
 //           trackOrderbutton(),
@@ -281,25 +84,9 @@ class UserProfile extends StatelessWidget {
 //           const SizedBox(height: 170),
 //           signOutButton()
 //         ],
-//       ),
+//       )
 //     );
 //   }
-
-//   Widget buildName(User user) => Column(
-//     children: [
-//       Text(
-//         user.fname + ' ' +  user.lname,
-//         style: TextStyle(
-//           fontWeight: FontWeight.bold,
-//           fontFamily: 'Poppins',
-//           color: cBlack,
-//           fontSize: 24
-//         ),
-//       ),
-//       const SizedBox(height: 4),
-//     ],
-//   );
-  
 
 //   //Track Order Button
 //   Widget trackOrderbutton() {
@@ -392,7 +179,7 @@ class UserProfile extends StatelessWidget {
 //   Widget signOutButton() {
 //     return TextButton(
 //       onPressed: () async {
-//         Navigator.push(context, MaterialPageRoute(builder: (context) => SignIn()));
+//         // await _auth.signOut();
 //       }, 
 //       child: Container(
 //         decoration: BoxDecoration(color: cGreen,
@@ -418,3 +205,215 @@ class UserProfile extends StatelessWidget {
 //     );
 //   }
 // }
+
+class UserProfile extends StatefulWidget {
+  static const String routeName = '/userprofilebuyer';
+
+  static Route route() {
+    return MaterialPageRoute(
+        settings: RouteSettings(name: routeName), builder: (_) => UserProfile());
+  }
+
+
+  @override
+  _UserProfileState createState() => _UserProfileState();
+}
+
+  bool loading = false;
+
+class _UserProfileState extends State<UserProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text("My Profile", 
+        style: TextStyle(
+            fontFamily: 'Poppins',
+            color: cGreen,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+                onPressed: () {
+                Navigator.pop(context);
+                },
+                  icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: cBlack,
+                  size: 15,
+                  ),
+                ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfile()));
+            },
+            color: cBlack,
+            icon: Icon(Icons.settings_outlined),
+          )
+        ],
+      ),
+      body: ListView(
+        // physics: BouncingScrollPhysics(),
+        children: [
+          // ProfileWidget(
+          //   imagePath: userOne.imagePath,
+          //   onClicked: () async {
+              
+          //   }
+          // ),
+          // const SizedBox(height: 24),
+          Column(
+            children: [
+              
+            ],
+          ),
+          const SizedBox(height: 24),
+          trackOrderbutton(),
+          const SizedBox(height: 10),
+          myFavsbutton(),
+          const SizedBox(height: 10),
+          startSellbutton(),
+          const SizedBox(height: 170),
+          signOutButton()
+        ],
+      ),
+    );
+  }
+
+  Widget buildName(User user) => Column(
+    children: [
+      Text(
+        user.fname + ' ' +  user.lname,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Poppins',
+          color: cBlack,
+          fontSize: 24
+        ),
+      ),
+      const SizedBox(height: 4),
+    ],
+  );
+  
+
+  //Track Order Button
+  Widget trackOrderbutton() {
+    return TextButton(
+      onPressed: () {
+
+      }, 
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Icon(Icons.location_on_outlined, color: cGreen),
+            SizedBox(width: 30),
+            Text(
+                "Track my Order",
+              style: TextStyle(fontSize: 20,
+              color: cBlack,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              )
+              ),
+          ],
+        ),
+        color: Colors.transparent,
+        width: double.infinity,
+        height: 65,
+      )
+    );
+  }
+
+  //Favorites Button
+  Widget myFavsbutton() {
+    return TextButton(
+      onPressed: () {
+
+      }, 
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Icon(Icons.favorite_border_rounded, color: cGreen),
+            SizedBox(width: 30),
+            Text(
+                "My Favorites",
+              style: TextStyle(fontSize: 20,
+              color: cBlack,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              )
+              ),
+          ],
+        ),
+        color: Colors.transparent,
+        width: double.infinity,
+        height: 65,
+      )
+    );
+  }
+
+//Start Selling Button
+  Widget startSellbutton() {
+    return TextButton(
+      onPressed: () {
+
+      }, 
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Icon(Icons.storefront_outlined, color: cGreen),
+            SizedBox(width: 30),
+            Text(
+                "Start Selling",
+              style: TextStyle(fontSize: 20,
+              color: cBlack,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              )
+              ),
+          ],
+        ),
+        color: Colors.transparent,
+        width: double.infinity,
+        height: 65,
+      )
+    );
+  }
+
+  //Sign Out Button
+  Widget signOutButton() {
+    return TextButton(
+      onPressed: () async {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => SignIn()));
+      }, 
+      child: Container(
+        decoration: BoxDecoration(color: cGreen,
+        borderRadius: BorderRadius.circular(12)),
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Icon(Icons.logout_rounded, color: cWhite),
+            SizedBox(width: 30),
+            Text(
+                "Sign Out",
+              style: TextStyle(fontSize: 20,
+              color: cWhite,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              )
+              ),
+          ],
+        ),
+        width: 350,
+        height: 65,
+      )
+    );
+  }
+}
