348b5c1ce998c66f4ffa03b9d6f2a4f606665220
MiraiKuriyama123 <enzoruiz0603@gmail.com>
2021-11-04 11:14:53 +0800

    Update messages.dart

--- croptivate_app/lib/widgets/messages.dart
+++ croptivate_app/lib/widgets/messages.dart
@@ -3,7 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatefulWidget {
-  const Messages({Key? key}) : super(key: key);
+  final sender;
+  final receiver;
+  final name;
+  const Messages(
+      {Key? key, required this.sender, required this.receiver, this.name})
+      : super(key: key);
 
   @override
   _MessagesState createState() => _MessagesState();
@@ -12,23 +17,43 @@ class Messages extends StatefulWidget {
 class _MessagesState extends State<Messages> {
   TextEditingController msg = TextEditingController();
   final StoreMessages = FirebaseFirestore.instance;
+  final Buyers = FirebaseFirestore.instance.collection('userBuyer');
   final auth = FirebaseAuth.instance;
+
+  Future getcontactname() async {
+    try {
+      Buyers.snapshots().forEach((element) {
+        element.docs.forEach((doc) {
+          print(doc.data());
+        });
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getcontactname();
     return Scaffold(
-      appBar:
-          AppBar(title: Text('Messages'), backgroundColor: Colors.green[400]),
+      appBar: AppBar(
+          title: Center(child: Text(widget.name)),
+          backgroundColor: Colors.green[400]),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-              height: 599,
-              child: SingleChildScrollView(
-                physics: ScrollPhysics(),
-                reverse: true,
-                child: Showmessages(),
-              )),
+            height: 390,
+            child: SingleChildScrollView(
+              physics: ScrollPhysics(),
+              reverse: true,
+              child: Showmessages(
+                sender: widget.sender,
+                receiver: widget.receiver,
+              ),
+            ),
+          ),
           Row(
             children: [
               Expanded(
@@ -44,8 +69,8 @@ class _MessagesState extends State<Messages> {
               ),
               IconButton(
                 onPressed: () {
-                  String contact = '5KEUrHxzWqWC9vPsosyykbGqj4c2';
-                  String sender = 'WAfFmobOMHgwsOy5x0X4djkAFha2';
+                  String contact = widget.receiver;
+                  String sender = widget.sender;
                   if (msg.text.isNotEmpty) {
                     StoreMessages.collection('Chats/' +
                             sender +
@@ -55,7 +80,19 @@ class _MessagesState extends State<Messages> {
                         .doc()
                         .set({
                       "message": msg.text.trim(),
-                      "sender": "enzo",
+                      "sender": widget.sender,
+                      "time": DateTime.now()
+                    });
+
+                    StoreMessages.collection('Chats/' +
+                            contact +
+                            '/Contacts/' +
+                            sender +
+                            '/Messages')
+                        .doc()
+                        .set({
+                      "message": msg.text.trim(),
+                      "sender": widget.sender,
                       "time": DateTime.now()
                     });
                   }
@@ -73,13 +110,15 @@ class _MessagesState extends State<Messages> {
 }
 
 class Showmessages extends StatelessWidget {
+  final sender;
+  final receiver;
+  const Showmessages({Key? key, required this.sender, required this.receiver})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
-    String contact = '5KEUrHxzWqWC9vPsosyykbGqj4c2';
-    String sender = 'WAfFmobOMHgwsOy5x0X4djkAFha2';
     return StreamBuilder<QuerySnapshot>(
       stream: FirebaseFirestore.instance
-          .collection('Chats/' + sender + '/Contacts/' + contact + '/Messages')
+          .collection('Chats/' + sender + '/Contacts/' + receiver + '/Messages')
           .orderBy('time')
           .snapshots(),
       builder: (context, snapshot) {
@@ -97,14 +136,14 @@ class Showmessages extends StatelessWidget {
               QueryDocumentSnapshot x = snapshot.data!.docs[i];
               return ListTile(
                 title: Column(
-                  crossAxisAlignment: 'enzo' == x['sender']
+                  crossAxisAlignment: sender == x['sender']
                       ? CrossAxisAlignment.end
                       : CrossAxisAlignment.start,
                   children: [
                     Container(
                         padding:
                             EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        color: 'enzo' == x['sender']
+                        color: sender == x['sender']
                             ? Colors.green[300]!.withOpacity(0.2)
                             : Colors.blue[300]!.withOpacity(0.2),
                         child: Text(x['message'])),
