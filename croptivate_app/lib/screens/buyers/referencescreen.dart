bf9dce7bd10bb7a5864f047c7579259d93fd816d
MiraiKuriyama123 <enzoruiz0603@gmail.com>
2021-11-29 18:03:44 +0800

    Update referencescreen.dart

--- croptivate_app/lib/screens/buyers/referencescreen.dart
+++ croptivate_app/lib/screens/buyers/referencescreen.dart
@@ -1,42 +1,130 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:croptivate_app/pallete.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ReferenceScreen extends StatelessWidget {
-  const ReferenceScreen({ Key? key }) : super(key: key);
+class ReferenceScreen extends StatefulWidget {
+  final dateordered;
+  const ReferenceScreen({Key? key, this.dateordered}) : super(key: key);
 
+  @override
+  _ReferenceScreenState createState() => _ReferenceScreenState();
   static const String routeName = '/reference';
   static Route route() {
     return MaterialPageRoute(
-        settings: RouteSettings(name: routeName), builder: (_) => ReferenceScreen());
+        settings: RouteSettings(name: routeName),
+        builder: (_) => ReferenceScreen());
+  }
+}
+
+class _ReferenceScreenState extends State<ReferenceScreen> {
+  final _auth = FirebaseAuth.instance;
+  String referenceID = '';
+  String buyer = '';
+  String seller = '';
+  String loc = '';
+  String PM = '';
+  String DO = '';
+
+  @override
+  void initState() {
+    super.initState();
+    findReference();
+  }
+
+  findReference() async {
+    await FirebaseFirestore.instance
+        .collection('Orders')
+        .get()
+        .then((querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        print(doc['Date ordered'].toString());
+        print(widget.dateordered);
+        if (doc['Date ordered'].toString() == widget.dateordered &&
+            _auth.currentUser!.uid == doc['Buyer']) {
+          print('meron');
+          setState(() {
+            buyer = doc['Buyer'];
+            loc = doc['location'];
+            PM = doc['Payment Method'];
+            DO = doc['Delivery Option'];
+            referenceID = doc.id;
+          });
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    print(referenceID);
+    print('loc');
     return Scaffold(
-backgroundColor: cWhite,
+      backgroundColor: cWhite,
       appBar: AppBar(
-            backgroundColor: cWhite,
-            elevation: 0,
-            title: Text(
-              "Reference Number",
+        backgroundColor: cWhite,
+        elevation: 0,
+        title: Text(
+          "Reference ID",
+          style: TextStyle(
+              color: cGreen,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Container(
+        child: Column(children: [
+          Center(
+            child: Text(
+              referenceID,
               style: TextStyle(
-                  color: cGreen,
                   fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1),
-            ),
-            centerTitle: true,
-            leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(
-                Icons.arrow_back_ios,
-                color: cBlack,
-                size: 15,
-              ),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
             ),
           ),
+          Center(
+            child: Text(
+              buyer,
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
+            ),
+          ),
+          Center(
+            child: Text(
+              PM,
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
+            ),
+          ),
+          Center(
+            child: Text(
+              DO,
+              style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black),
+            ),
+          )
+        ]),
+      ),
+      bottomNavigationBar: TextButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: Text('OK'),
+      ),
     );
   }
 }
