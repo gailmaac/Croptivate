8b2baa7b0cb071aa0c586d319ad8f898fdc1b009
gailmaac <gailmac245@gmail.com>
2021-11-30 20:44:16 +0800

    Update user.dart

--- croptivate_app/lib/models/user.dart
+++ croptivate_app/lib/models/user.dart
@@ -14,10 +14,9 @@ class UserData {
   final String lname;
   final String loc;
   final String shopname;
-  final String shopdesc;
   final int cnum;
   final String avatar;
 
-  UserData({ required this.uid, required this.sellerType, required this.fname, required this.lname, required this.loc, required this.shopname, required this.shopdesc, required this.cnum, required this.avatar});
+  UserData({ required this.uid, required this.sellerType, required this.fname, required this.lname, required this.loc, required this.shopname, required this.cnum, required this.avatar});
 
 }
