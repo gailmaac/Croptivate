92e015af5f96a06ac265c30642fc0f10a2c05dd7
MiraiKuriyama123 <enzoruiz0603@gmail.com>
2021-12-07 17:30:10 +0800

    Update messagesscreen.dart

--- croptivate_app/lib/widgets/messagesscreen.dart
+++ croptivate_app/lib/widgets/messagesscreen.dart
@@ -146,13 +146,12 @@ class ShowContacts extends StatelessWidget {
                     child: ListTile(
                       leading: CircleAvatar(
                         radius: 80.0,
-                        child: ClipOval(
-                            child: Image.network(
+                        child: Image.network(
                           profilepic,
                           fit: BoxFit.cover,
-                          width: 160.0,
-                          height: 160.0,
-                        )),
+                          width: 80.0,
+                          height: 80.0,
+                        ),
                       ),
                       onTap: () async {
                         await FirebaseFirestore.instance
