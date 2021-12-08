8775c619e077314ef37521347c74711f0273f9c7
MiraiKuriyama123 <enzoruiz0603@gmail.com>
2021-12-08 18:35:05 +0800

    Update home_seller.dart

--- croptivate_app/lib/screens/sellers/home_seller.dart
+++ croptivate_app/lib/screens/sellers/home_seller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:croptivate_app/pallete.dart';
 import 'package:croptivate_app/screens/buyers/user_profile.dart';
 import 'package:croptivate_app/screens/sellers/add_product.dart';
@@ -26,8 +27,54 @@ List<Tab> tabs = [
 ];
 
 class _HomeSellerState extends State<HomeSeller> {
+  List shipping = [];
+  List to_ship = [];
+  List refunded = [];
+  List completed = [];
+
+  getOrders() async {
+    List _to_ship = [];
+    List _shipping = [];
+    List _refunded = [];
+    List _completed = [];
+    try {
+      await FirebaseFirestore.instance
+          .collection('Orders')
+          .get()
+          .then((querySnapshot) {
+        querySnapshot.docs.forEach((doc) {
+          print(doc['To Ship']);
+          if (doc['To Ship'] == 'true') {
+            _to_ship.add(doc.data());
+          }
+          if (doc['Shipping'] == 'true') {
+            _shipping.add(doc.data());
+          }
+          if (doc['Refunded'] == 'true') {
+            _refunded.add(doc.data());
+          }
+          /*if (doc['Completed'] == 'true') {
+            _completed.add(doc.data());
+          }*/
+        });
+      });
+
+      setState(() {
+        to_ship = _to_ship;
+        shipping = _shipping;
+        refunded = _refunded;
+        completed = _completed;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('hala pano to');
+    getOrders();
+    print(to_ship.length);
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
@@ -62,19 +109,55 @@ class _HomeSellerState extends State<HomeSeller> {
         body: TabBarView(children: [
           Container(
             height: double.infinity,
-            color: cWhite,
+            child: ListView.builder(
+                itemCount: to_ship.length,
+                itemBuilder: (context, int) {
+                  return ListTile(
+                    tileColor: cGrey,
+                    title: Text(to_ship[int]['Date ordered'].toString()),
+                    trailing: Text(to_ship[int]['Delivery Option'].toString()),
+                  );
+                }),
           ),
           Container(
             height: double.infinity,
             color: cWhite,
+            child: ListView.builder(
+                itemCount: shipping.length,
+                itemBuilder: (context, int) {
+                  return ListTile(
+                    tileColor: cGrey,
+                    title: Text(shipping[int]['Date ordered'].toString()),
+                    trailing: Text(shipping[int]['Delivery Option'].toString()),
+                  );
+                }),
           ),
           Container(
             height: double.infinity,
             color: cWhite,
+            child: ListView.builder(
+                itemCount: completed.length,
+                itemBuilder: (context, int) {
+                  return ListTile(
+                    tileColor: cGrey,
+                    title: Text(completed[int]['Date ordered'].toString()),
+                    trailing:
+                        Text(completed[int]['Delivery Option'].toString()),
+                  );
+                }),
           ),
           Container(
             height: double.infinity,
             color: cWhite,
+            child: ListView.builder(
+                itemCount: refunded.length,
+                itemBuilder: (context, int) {
+                  return ListTile(
+                    tileColor: cGrey,
+                    title: Text(refunded[int]['Date ordered'].toString()),
+                    trailing: Text(refunded[int]['Delivery Option'].toString()),
+                  );
+                }),
           ),
         ]),
         bottomNavigationBar: Container(
@@ -113,7 +196,10 @@ class _HomeSellerState extends State<HomeSeller> {
                 ),
                 IconButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => UserProfile()));
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => UserProfile()));
                     },
                     icon: Icon(
                       Icons.person_outline_rounded,
