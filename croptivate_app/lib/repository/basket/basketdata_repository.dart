60458ecf7b8dbf68609418443b95c02f10e692ef
gailmaac <gailmac245@gmail.com>
2021-12-15 00:26:54 +0800

    Update basketdata_repository.dart

--- croptivate_app/lib/repository/basket/basketdata_repository.dart
+++ croptivate_app/lib/repository/basket/basketdata_repository.dart
@@ -1,16 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:croptivate_app/models/basketdata_model.dart';
 import 'package:croptivate_app/repository/basket/base_basketdata_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class BasketDataRepository extends BaseBasketDataRepository {
 
   final FirebaseFirestore _firebaseFirestore;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   BasketDataRepository({FirebaseFirestore? firebaseFirestore,}) : _firebaseFirestore = firebaseFirestore ?? FirebaseFirestore.instance;
 
   @override
   Future<void> addBasket(BasketData basketData) {
-    return _firebaseFirestore.collection('basket').add(basketData.toDocument());
+    var uid = _auth.currentUser!.uid;
+    return _firebaseFirestore.collection('basket').doc(uid).set(basketData.toDocument());
   }
 
 }
