08c690d5583b2f430b69a03a58a3bcdc526ed32d
MiraiKuriyama123 <enzoruiz0603@gmail.com>
2021-12-21 05:55:56 +0800

    Update ordersummary.dart

--- croptivate_app/lib/widgets/ordersummary.dart
+++ croptivate_app/lib/widgets/ordersummary.dart
@@ -1,86 +1,104 @@
 import 'package:croptivate_app/blocs/basket/basket_bloc.dart';
 import 'package:croptivate_app/models/basket_model.dart';
+import 'package:croptivate_app/models/product_model.dart';
 import 'package:croptivate_app/pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class OrderSummary extends StatelessWidget {
-  const OrderSummary({Key? key}) : super(key: key);
+class OrderSummary extends StatefulWidget {
+  final selectedproducts;
+  final selectedproductvalues;
+  const OrderSummary(
+      {Key? key, this.selectedproducts, this.selectedproductvalues})
+      : super(key: key);
+
+  @override
+  State<OrderSummary> createState() => _OrderSummaryState();
+}
+
+class _OrderSummaryState extends State<OrderSummary> {
+  double subtotal = 0;
+  double total = 0;
+
+  computetotal() {
+    List<Product> selectedproducts = widget.selectedproducts;
+    List<int> selectedvalues = widget.selectedproductvalues;
+    double _subtotal = 0;
+    for (int x = 0; x < selectedproducts.length; x++) {
+      _subtotal += selectedproducts[x].price * selectedvalues[x];
+    }
+    setState(() {
+      subtotal = _subtotal;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<BasketBloc, BasketState>(
-      builder: (context, state) {
-        if (state is BasketLoaded) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Total".toUpperCase(),
-                        style: TextStyle(
-                            color: cBlack,
-                            fontSize: 24,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.w900)),
-                    SizedBox(width: 110),
-                    Text('\₱${state.basket.subtotalString}',
-                        style: TextStyle(
-                            color: cBlack,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold)),
-                  ],
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Text("Shipping Fee".toUpperCase(),
-                    //     style: TextStyle(
-                    //         color: cBlack,
-                    //         fontSize: 18,
-                    //         fontFamily: 'Poppins',
-                    //         fontWeight: FontWeight.bold)),
-                    // Text('\₱${state.basket.shippingFeeString}',
-                    //     style: TextStyle(
-                    //         color: cBlack,
-                    //         fontSize: 18,
-                    //         fontWeight: FontWeight.bold)),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                // Row(
-                //   crossAxisAlignment: CrossAxisAlignment.start,
-                //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //   children: [
-                //     Text("Total".toUpperCase(),
-                //         style: TextStyle(
-                //             color: Colors.black,
-                //             fontSize: 24,
-                //             fontFamily: 'Poppins',
-                //             fontWeight: FontWeight.w900)),
-                //     Text('\₱${state.basket.totalString}',
-                //         style: TextStyle(
-                //             color: Colors.black,
-                //             fontSize: 24,
-                //             fontWeight: FontWeight.bold)),
-                //   ],
-                // )
-              ],
-            ),
-          );
-        } else {
-          return Text("Something went wrong");
-        }
-      },
+    computetotal();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Total".toUpperCase(),
+                  style: TextStyle(
+                      color: cBlack,
+                      fontSize: 24,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w900)),
+              SizedBox(width: 90),
+              Text('\₱ ' + subtotal.toStringAsFixed(2),
+                  style: TextStyle(
+                      color: cBlack,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold)),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Text("Shipping Fee".toUpperCase(),
+              //     style: TextStyle(
+              //         color: cBlack,
+              //         fontSize: 18,
+              //         fontFamily: 'Poppins',
+              //         fontWeight: FontWeight.bold)),
+              // Text('\₱${state.basket.shippingFeeString}',
+              //     style: TextStyle(
+              //         color: cBlack,
+              //         fontSize: 18,
+              //         fontWeight: FontWeight.bold)),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //   children: [
+          //     Text("Total".toUpperCase(),
+          //         style: TextStyle(
+          //             color: Colors.black,
+          //             fontSize: 24,
+          //             fontFamily: 'Poppins',
+          //             fontWeight: FontWeight.w900)),
+          //     Text('\₱${state.basket.totalString}',
+          //         style: TextStyle(
+          //             color: Colors.black,
+          //             fontSize: 24,
+          //             fontWeight: FontWeight.bold)),
+          //   ],
+          // )
+        ],
+      ),
     );
   }
 }
